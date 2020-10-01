be6f218d239efb7eb42b66a149acf9f981dc8ed4
wook2124 <wook2124@naver.com>
2020-10-01 19:49:20 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("hello")
+people = [{'name': 'bob', 'age': 20}, 
+          {'name': 'carry', 'age': 38},
+          {'name': 'john', 'age': 7},
+          {'name': 'smith', 'age': 17},
+          {'name': 'ben', 'age': 27}]
+
+
+for person in people:
+    if person['age'] >= 20:
+        print(person['name'])
