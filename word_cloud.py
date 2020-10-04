71636b9bdda1c141c017e698e2690ccb6696f8d7
wook2124 <wook2124@naver.com>
2020-10-04 21:52:16 +0900

    Update word_cloud.py

--- word_cloud.py
+++ word_cloud.py
@@ -6,13 +6,13 @@ with open("KaKaoTalk 2.txt", "r", encoding="utf-8") as f:
     lines = f.readlines()
     for line in lines[5:]:
         if '] [' in line:
-            text += line.split('] ')[2].replace('ㅋ', '').replace('ㅠ', '').replace('ㅜ', '').replace('이모티콘\n', '').replace('사진\n', '')
+            text += line.split('] ')[2].replace('ㅋ', '').replace('ㅠ', '').replace('ㅜ', '').replace('이모티콘\n', '').replace('사진\n', '').replace('ㅇ', '').replace('ㅎ', '')
 
 # print(text)
 
 wc = WordCloud(font_path='C:/Users/wook2/AppData/Local/Microsoft/Windows/Fonts/NanumBarunGothicBold.ttf', background_color="white", width=600, height=400)
 wc.generate(text)
-wc.to_file("result 2.png")
+wc.to_file("result.png")
 
 # # 폰트가 저장된 위치 확인하기
 # import matplotlib.font_manager as fm
