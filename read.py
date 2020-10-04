7b4a4f5210991faf13793beb61dd6e7a33562ab5
wook2124 <wook2124@naver.com>
2020-10-04 21:07:52 +0900

    Change files

--- read.py
+++ read.py
@@ -6,3 +6,16 @@ with open("test.txt", "r", encoding="utf-8") as f:
         text += line
 
 print(text)
+
+from wordcloud import WordCloud
+
+wc = WordCloud(font_path='C:/Users/wook2/AppData/Local/Microsoft/Windows/Fonts/NanumBarunGothicBold.ttf', background_color="white", width=600, height=400)
+wc.generate(text)
+wc.to_file("result.png")
+
+# import matplotlib.font_manager as fm
+
+# # 이용 가능한 폰트 중 '고딕'만 선별
+# for font in fm.fontManager.ttflist:
+#     if 'Gothic' in font.name:
+#         print(font.name, font.fname)
