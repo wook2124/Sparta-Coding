44e8f1ec55812457be41bacdf551e106299cdcd4
wook2124 <wook2124@naver.com>
2020-12-27 01:57:17 +0900

    JavaScript
    추가로 히든 메시지를 넣어서 블로그로 이동되게끔 설정함

--- HTML, CSS/Day 2/open.js
+++ HTML, CSS/Day 2/open.js
@@ -1,4 +1,9 @@
 function open_letter() {
-    document.getElementsByClassName("letter-close")[0].style.display = 'none'
-    document.getElementsByClassName("letter-open")[0].style.display = 'block'
+  document.getElementsByClassName("letter-close")[0].style.display = "none";
+  document.getElementsByClassName("letter-open")[0].style.display = "block";
+}
+
+function go_rtan() {
+    alert('앗, 어떻게 찾았지!')
+    window.location.href='https://blog.naver.com/wook2124'
 }
