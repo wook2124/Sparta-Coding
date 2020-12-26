7a7106d7de51fc3d75052c25057551fc158f6923
wook2124 <wook2124@naver.com>
2020-12-27 01:52:50 +0900

    Javascript
    편지 봉투를 여는 Open 기능과 눈내리는 라이브러리를 추가함

---
+++ HTML, CSS/Day 2/open.js
@@ -0,0 +1,4 @@
+function open_letter() {
+    document.getElementsByClassName("letter-close")[0].style.display = 'none'
+    document.getElementsByClassName("letter-open")[0].style.display = 'block'
+}
